0c4c5929b0bf234a2c976fbc450e1f3d54421d9d
LotteWestLakehanoiFan <ALotteFan81@gmail.com>
2023-10-25 23:22:00 +0700

    Attempt installation setup procedure

--- scripts/common_member_db_meta.js
+++ scripts/common_member_db_meta.js
@@ -1,6 +1,14 @@
 const DB_META = {};
 
-function set_DB_META(meta){
+function set_DB_META(meta) {
     DB_META.name = "MEMBERS_DB";
     DB_META.version = meta.latestVersion;
+}
+
+function redirectToMembersDB_reader() {
+    location.href = "MembersDB_reader.html";
+}
+
+function redirectToMembers_DB(){
+    location.href="MEMBERS_DB.html";
 }
