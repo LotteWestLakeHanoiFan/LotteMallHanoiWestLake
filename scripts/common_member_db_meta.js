823c304f5fc6ba974278869458650cd4b529aec6
LotteWestLakehanoiFan <ALotteFan81@gmail.com>
2023-11-01 12:25:45 +0700

    Update ReportHelper nolonger display require patrol position to generate report fix unstable updating Members_DB & MembersDB_reader

--- scripts/common_member_db_meta.js
+++ scripts/common_member_db_meta.js
@@ -1,5 +1,15 @@
 const DB_META = {};
 
+const membersDB_Meta_Relative_URL = "MembersDB_Meta.json"
+async function get_latest_db_version(onSuccessCallback){
+    let response = await fetch(membersDB_Meta_Relative_URL)
+    if (response.ok){
+        onSuccessCallback(await response.json());
+    } else {
+        alert(`HTTP-Error: ${response.status}`);
+    }
+}
+
 function set_DB_META(meta) {
     DB_META.name = "MEMBERS_DB";
     DB_META.version = meta.latestVersion;
@@ -14,4 +24,4 @@ function redirectToMembers_DB(){
 }
 
 
-get_latest_db_version(set_DB_META)
+
--- scripts/members_db_version_reader.js
+++
@@ -1,9 +0,0 @@
-const membersDB_Meta_Relative_URL = "MembersDB_Meta.json"
-async function get_latest_db_version(onSuccessCallback){
-    let response = await fetch(membersDB_Meta_Relative_URL)
-    if (response.ok){
-        onSuccessCallback(await response.json());
-    } else {
-        alert(`HTTP-Error: ${response.status}`);
-    }
-}
